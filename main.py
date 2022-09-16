eafa9c7d56e154ce02e03f393ffda51b0778e8aa
rlp81 <58034254+rlp81@users.noreply.github.com>
2022-09-15 21:46:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import configparser
 config = configparser.ConfigParser()
 confile = config.read("config.conf")
-bot = discord.Bot(debug_guilds=[806540680473346058])
+bot = discord.Bot(debug_guilds=[])
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
@@ -15,6 +15,30 @@ for file in os.listdir(f"{os.getcwd()}/cogs"):
             print(f"cogs.{file[:-3]} loadeded successfully")
         except:
             print(f"cogs.{file[:-3]} failed to load")
+
+@bot.slash_command()
+async def load(context, extintion):
+    try:
+        bot.load_extension(f'cogs.{extintion}')
+        await context.send(f"cogs.{extintion} loaded sucessfully!")
+    except:
+        await context.send(f"cogs.{extintion} failed to load!")
+@bot.slash_command()
+async def unload(context, extintion):
+    try:
+        bot.unload_extension(f'cogs.{extintion}')
+        await context.send(f"cogs.{extintion} unloaded sucessfully!")
+    except:
+        await context.send(f"cogs.{extintion} failed to unload!")
+
+@bot.slash_command(name="reload", description="Reload a cog")
+async def reload(context, extintion):
+        try:
+            bot.unload_extension(f'cogs.{extintion}')
+            bot.load_extension(f'cogs.{extintion}')
+            await context.send(f"cogs.{extintion} reloaded sucessfully!")
+        except:
+            await context.send(f"cogs.{extintion} failed to reload!")
 
 if __name__ == "__main__":
     bot.run(config.get("config","token"))
